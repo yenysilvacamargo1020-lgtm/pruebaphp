38ee92b797a3fba378513cd03cc877397a92fdb1
Yeny <yenysilvacamargo1020@gmail.com>
2025-08-29 20:33:59 -0500

    actualizacion xapp categorias

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS tienda2;
+CREATE DATABASE IF NOT EXISTS tienda14;
 
-USE tienda2;
+USE tienda14;
 
 CREATE TABLE IF NOT EXISTS t_usuarios(
     id_usuarios      int (11)        auto_increment NOT NUll,
@@ -16,3 +16,23 @@ CREATE TABLE IF NOT EXISTS t_usuarios(
 
 INSERT INTO t_usuarios   VALUE(NULL,'yeny','silva','yenysilvacamargo1020@gmail.com','123456789','admin','imagen');
 
+CREATE TABLE if NOT EXISTS t_categorias(
+    id_categoria  int(11)   auto_increment NOT NULL,
+    nombre_categoria  varchar(100) NOT NULL,
+    CONSTRAINT pk_categoria    PRIMARY KEY (id_categoria)
+)ENGINE=InnoDb;
+INSERT INTO t_categorias VALUE(NULL,'construccion'),(NUll,'electricos'),(NUll,'pinturas'),(NUll,'pisos')
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto        int(11) auto_increment NOT NULL,
+    nombre_producto    varchar(100)           NOT NULL,
+    descripcion    varchar(200)     NOT NULL,
+    fecha      date    NOT NULL,
+    stock              int(100)               NOT NUll,
+    precio             float(10,2)             NOT NUll,
+    imagen             varchar(255)      NOT NUll,
+    oferta             varchar(200)     NOT NUll,
+    id_categoria    int(11)    NOT NUll,
+    CONSTRAINT pk_producto    PRIMARY KEY (id_producto),
+    CONSTRAINT fk_producto_categoria FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDb;
