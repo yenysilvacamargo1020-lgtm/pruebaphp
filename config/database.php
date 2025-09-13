5d49d1a6fd1761d6ef99af638375a8b4cb1ce747
yeny silva <yenysilvacamargo1020@gmail.com>
2025-09-12 20:48:31 -0500

    Actualizacion 12 de septiembre

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+class database{
+    public static function conectar(){
+        $conexion = new mysqli("localhost","root","","tienda14");
+        $conexion->query("SET NAMES 'utf-8'");
+    }
+}
---
+++ models/UsuarioModel.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'config/database.php';
+class Usuario{
+    public $db;
+    public function _construct(){
+        $this ->db = database::conectar();
+    }
+    public $id_usuario;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $rol;
+    public $imagen;
+
+    function getId_usuario(){
+        return $this ->id_usuario;
+    }
+    function getId_nombre(){
+        return $this ->nombre;
+    }
+    function getapellido(){
+        return $this ->apellido;
+    }
+    function getemail(){
+        return $this ->email;
+    }
+    function getpassword(){
+        return $this ->password;
+    }
+    function getimagen(){
+        return $this ->imagen;
+    }
+
+    function setId_usuario($id_usuario){
+        $this ->id_usuario= $id_usuario;
+    }
+    function setNombre($nombre){
+        $this ->nombre = $nombre;
+    }
+    function setapellido($apellido){
+        $this ->apellido = $apellido;
+    }
+    function setemail($email){
+        $this ->email= $email;
+    }
+    function setpassword($password){
+        $this ->password= $password;
+    }
+    function setimagen($imagen){
+        $this ->imagen= $imagen;
+    }
+
+
+}
